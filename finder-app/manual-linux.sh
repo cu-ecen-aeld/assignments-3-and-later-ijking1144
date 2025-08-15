d714aa58dc63228ed0717d7780eae2ddc1b2bb87
ted <ted@School.myguest.virtualbox.org>
2025-08-15 16:43:28 -0400

    7

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -60,10 +60,9 @@ fi
 # TODO: Create necessary base directories
 mkdir ${OUTDIR}/rootfs
 cd ${OUTDIR}/rootfs
-mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir -p bin dev etc conf home lib lib64 proc sbin sys tmp usr var
 mkdir -p usr/bin usr/lib usr/sbin
 mkdir -p var/log
-mkdir -p home/conf
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
