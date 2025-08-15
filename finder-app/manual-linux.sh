3bc4d61060fd8454edc4011fbf5143947dde8d5e
ted <ted@School.myguest.virtualbox.org>
2025-08-15 14:14:23 -0400

    Part 2 REL1

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -35,10 +35,20 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
+    #Cleans the config files
+    make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- mrproper
+    #Configures default config for virt dev board
+    make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- defconfig
+    #Makes the kernel image
+    make -j4 ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- 
+    #makes all the modules
+    make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- modules
+    #Makes the device tree
+    make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- dtbs
 fi
 
 echo "Adding the Image in outdir"
-
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}/
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
 if [ -d "${OUTDIR}/rootfs" ]
@@ -48,6 +58,11 @@ then
 fi
 
 # TODO: Create necessary base directories
+mkdir ${OUTDIR}/rootfs
+cd ${OUTDIR}/rootfs
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -55,26 +70,56 @@ then
 git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
-    # TODO:  Configure busybox
+    # TODO:  Configure busyboxs
+    make defconfig
 else
     cd busybox
 fi
 
 # TODO: Make and install busybox
+make distclean
+make defconfig
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 
 echo "Library dependencies"
+cd ${OUTDIR}/rootfs
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
+cd ${OUTDIR}
 
 # TODO: Add library dependencies to rootfs
+TOOLCHAIN=/home/ted/XCompile/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu
+cp -a $TOOLCHAIN/libc/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib/
+cp -a $TOOLCHAIN/libc/lib64/libm.so.6 ${OUTDIR}/rootfs/lib64/
+cp -a $TOOLCHAIN/libc/lib64/libresolv.so.2 ${OUTDIR}/rootfs/lib64/
+cp -a $TOOLCHAIN/libc/lib64/libc.so.6 ${OUTDIR}/rootfs/lib64/
 
 # TODO: Make device nodes
-
+cd ${OUTDIR}/rootfs
+if [ ! -c dev/null ]; then
+	sudo mknod -m 666 dev/null c 1 3
+fi
+if [ ! -c dev/console ]; then
+	sudo mknod -m 666 dev/console c 5 1 
+fi
 # TODO: Clean and build the writer utility
+cd ${FINDER_APP_DIR}
+make clean
+make CROSS_COMPILE=${CROSS_COMPILE}
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
 
+cp finder-test.sh ${OUTDIR}/rootfs/home
+cp writer ${OUTDIR}/rootfs/home
+cp finder.sh ${OUTDIR}/rootfs/home
+cp autorun-qemu.sh ${OUTDIR}/rootfs/home
+
 # TODO: Chown the root directory
 
+sudo chown -R root:root ${OUTDIR}/rootfs
+
 # TODO: Create initramfs.cpio.gz
+cd "$OUTDIR/rootfs"
+find . | cpio -H newc -ov --owner root:root | gzip > "${OUTDIR}/initramfs.cpio.gz"
