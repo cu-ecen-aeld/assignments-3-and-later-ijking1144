343a91382cc2bd8622f7cd4da1f1b5b0051488e0
ted <ted@School.myguest.virtualbox.org>
2025-08-16 14:01:20 -0400

    2

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -10,24 +10,52 @@
 
 void* threadfunc(void* thread_param)
 {
-
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
-    //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    thread_func_args->thread_complete_success=false;
+    usleep((thread_func_args->wait_to_obtain_ms)*1000);
+    int mut_state=pthread_mutex_lock(thread_func_args->mutex);
+	if (mut_state!=0){
+		printf("Mutex Lock failed");
+		return thread_param;
+	}
+	
+	else{
+		mut_state=0;
+		usleep((thread_func_args->wait_to_release_ms)*1000);
+		int mut_state=pthread_mutex_unlock(thread_func_args->mutex);
+		if (mut_state==0){
+			thread_func_args->thread_complete_success=true;
+		}
+	}
     return thread_param;
 }
 
 
 bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int wait_to_obtain_ms, int wait_to_release_ms)
 {
+
+	struct thread_data * thread_data = malloc(sizeof(struct thread_data));
+	thread_data->mutex = mutex;
+    	thread_data->wait_to_obtain_ms = wait_to_obtain_ms;
+    	thread_data->wait_to_release_ms = wait_to_release_ms;
+    	thread_data->thread_complete_success = false;
+    	
+    	int thread_status=pthread_create(thread,NULL,threadfunc,thread_data);
+    		if (thread_status!=0){
+    			free(thread_data);
+    			return false;
+    		}
+    	return true;
     /**
      * TODO: allocate memory for thread_data, setup mutex and wait arguments, pass thread_data to created thread
-     * using threadfunc() as entry point.
+     * using threadfunc() as aentry point.
      *
-     * return true if successful.
+     * return true aif successful.
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+    
 }
 
